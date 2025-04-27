259dbf8dcb21345a34003a5e43280163a185561b
sayeemx <141837085+sprov007@users.noreply.github.com>
2025-04-27 11:36:09 +0000

    Update server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ const authMiddleware = (req, res, next) => {
   if (!token) return res.status(401).json({ message: 'No token, access denied.' });
 
   try {
-    const decoded = jwt.verify(token, process.env.require('crypto').randomBytes(64).toString('hex');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET;
     req.user = decoded;
     next();
   } catch (error) {
