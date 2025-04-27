6c24de7243d72b84744d4829843f371584de0c02
sayeemx <141837085+sprov007@users.noreply.github.com>
2025-04-27 11:27:37 +0000

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post('/login', async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid credentials!' });
 
-    const token = jwt.sign({ id: user._id }, process.env.require('crypto').randomBytes(64).toString('hex'), { expiresIn: '1h' });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
     res.json({ token, user: { id: user._id, username: user.username, email: user.email } });
   } catch (error) {
     console.error(error);
