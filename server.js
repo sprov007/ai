2c33d18febeff7ccfc22a9d25dba47f78ac4756c
sayeemx <141837085+sprov007@users.noreply.github.com>
2025-04-27 11:09:26 +0000

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const User = require('./models/User');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.10000 || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Update CORS configuration
 app.use(cors({
